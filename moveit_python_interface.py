dbb5f31421592e9ce553373251f9c0ea4348aeaf
isaimat <32847184+isaimat@users.noreply.github.com>
2021-03-14 18:01:32 -0700

    Final Update for Moveit_python_interface

--- moveit_python_interface.py
+++ moveit_python_interface.py
@@ -5,11 +5,15 @@ import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
-import geometry_msgs.msg
+import geometry_msgs.msg 
+#----------------------------------------------------------------------------------
+from std_msgs.msg import Int32 # Messages used in the node must be imported.
+from geometry_msgs.msg import Pose # new import for opencv communication
+
+#----------------------------------------------------------------------------------
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
-## END_SUB_TUTORIAL
 
 def all_close(goal, actual, tolerance):
   """
@@ -33,15 +37,26 @@ def all_close(goal, actual, tolerance):
 
   return True
 
-class MoveGroupPythonIntefaceTutorial(object):
-  """MoveGroupPythonIntefaceTutorial"""
+class MoveGroupPythonInteface_RoboticArm_Rescue(object): 
+  """MoveGroupPythonInteface_RoboticArm_Rescue"""
+
+  def callback1(self):
+    self.x = geometry_msgs.msg.Pose.position.x
+    self.y = geometry_msgs.msg.Pose.position.y
+    rospy.loginfo("Recieve data from x: %f, y: %f", geometry_msgs.msg.Pose.position.x,geometry_msgs.msg.Pose.position.y)
+    return self.x, self.y
+  
+  
   def __init__(self):
-    super(MoveGroupPythonIntefaceTutorial, self).__init__()
+    super(MoveGroupPythonInteface_RoboticArm_Rescue, self).__init__() 
  
     ## First initialize `moveit_commander`_ and a `rospy`_ node:
     moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_group_python_interface_tutorial',
+#-------------------------------------------------------------------
+    rospy.init_node('move_group_python_interface',
                     anonymous=True)
+    rospy.Subscriber("follow_blob",Pose,self.callback1)
+#-------------------------------------------------------------------
 
     ## Get the name of the robot - this will be used to properly define the end-effector link when adding a box
     robot_name = rospy.get_param("~robot_name")
@@ -93,7 +108,7 @@ class MoveGroupPythonIntefaceTutorial(object):
     print "============ Printing robot state"
     print robot.get_current_state()
     print ""
-    ## END_SUB_TUTORIAL
+    ## END
 
     # Misc variables
     self.box_name = ''
@@ -105,22 +120,22 @@ class MoveGroupPythonIntefaceTutorial(object):
     self.eef_link = eef_link
     self.group_names = group_names
 
-  def go_to_joint_state(self, joint0, joint1,joint2, joint3, joint4):
- 
+  def go_to_joint_state(self, joint0, joint1, joint2, joint3, joint4):
+  
     group = self.group
     dof = self.dof
-   
+    
     ## Planning to a Joint Goal
    
     joint_goal = group.get_current_joint_values()
-    joint_goal[0] = joint0      #[0,-pi/4,0,-pi/2,0,Pi/3]
+    joint_goal[0] = joint0      
     joint_goal[1] = joint1
-    joint_goal[2] = joint2
+    joint_goal[2] = joint2 
     joint_goal[3] = joint3
     if dof >= 5:
         joint_goal[4] = joint4
     if dof >= 6:
-        joint_goal[5] = 0
+        joint_goal[5] = joint6
 
     # The go command can be called with joint values, poses, or without any
     # parameters if you have already set the pose or joint target for the group
@@ -129,21 +144,63 @@ class MoveGroupPythonIntefaceTutorial(object):
     # Calling ``stop()`` ensures that there is no residual movement
     group.stop()
 
-    ## END_SUB_TUTORIAL
+    ## END
 
     # For testing:
     # we use the class variable rather than the copied state variable
     current_joints = self.group.get_current_joint_values()
     return all_close(joint_goal, current_joints, 0.01)
 
+  def scoop_left(self,j0, j1, j2, j3, j4):
+    Movement = MoveGroupPythonInteface_RoboticArm_Rescue()# recognizing because class must be initialized again 
+   # Joint initialization
+#------------------------- Saving object from water on the Left side of container---------------------- 
+    j4 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j0=pi/4
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j4=0
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3=0
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j0= -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j4 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    return
 
+  def scoop_right(self,j0, j1, j2, j3, j4):
+
+    Movement = MoveGroupPythonInteface_RoboticArm_Rescue()# recognizing because class must be initialized again ********
+   # Joint initialization
+#------------------------- Saving object from water on the rightside of container---------------------- 
+    j4 = pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j0=-pi/3
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j4=0
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3=0
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j0= -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j3 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    j4 = -pi/2
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
+    return
 
 
   def go_to_pose_goal(self):
-
     group = self.group
     robot_name = self.robot_name
-   
+    
     ## Planning to a Pose Goal
     ## ^^^^^^^^^^^^^^^^^^^^^^^
     ## We can plan a motion for this group to a desired pose for the
@@ -154,8 +211,8 @@ class MoveGroupPythonIntefaceTutorial(object):
         pose_goal.position.x = 0.1
         pose_goal.position.z = 0.15
     else:
-        pose_goal.position.x = 0.25
-        pose_goal.position.z = 0.28
+        pose_goal.position.x = 0.15
+        pose_goal.position.z = 0.24
     group.set_pose_target(pose_goal)
 
     ## Now, we call the planner to compute the plan and execute it.
@@ -165,37 +222,29 @@ class MoveGroupPythonIntefaceTutorial(object):
     # It is always good to clear your targets after planning with poses.
     # Note: there is no equivalent function for clear_joint_value_targets()
     group.clear_pose_targets()
-
-   
     # For testing:
     # we use the class variable rather than the copied state variable
     current_pose = self.group.get_current_pose().pose
     return all_close(pose_goal, current_pose, 0.01)
 
 
-  def plan_cartesian_path(self, d_x, d_y, d_z):
-
+  def plan_cartesian_path(self,d_x,d_y,d_z):
     group = self.group
-    ## Cartesian Paths
-    ## ^^^^^^^^^^^^^^^
     ## You can plan a Cartesian path directly by specifying a list of waypoints
     ## for the end-effector to go through:
     ##
+    scale = 0.01
     waypoints = []
-
-    scale=0.01
+    
     wpose = group.get_current_pose().pose
-    wpose.position.z += scale *d_z  # First move up (z)
+    wpose.position.y += scale*d_y 
     waypoints.append(copy.deepcopy(wpose))
 
-    wpose.position.y += scale *d_y  # Second move forward in (y) -11.25
+    wpose.position.z -= scale * d_z  
     waypoints.append(copy.deepcopy(wpose))
 
-    wpose.position.x += scale *d_x  # Second move forward in (x) 10.8
+    wpose.position.x += scale*d_x  
     waypoints.append(copy.deepcopy(wpose))
-   
-    #wpose.position.z = scale * 0  # Third move down (z)
-    #waypoints.append(copy.deepcopy(wpose))
 
     # We want the Cartesian path to be interpolated at a resolution of 1 cm
     # which is why we will specify 0.01 as the eef_step in Cartesian
@@ -208,15 +257,11 @@ class MoveGroupPythonIntefaceTutorial(object):
     # Note: We are just planning, not asking move_group to actually move the robot yet:
     return plan, fraction
 
-    ## END_SUB_TUTORIAL
-
+    ## END
   def display_trajectory(self, plan):
 
     robot = self.robot
     display_trajectory_publisher = self.display_trajectory_publisher
-
-    ## BEGIN_SUB display_trajectory
-    ##
     ## Displaying a Trajectory
     ## ^^^^^^^^^^^^^^^^^^^^^^^
     ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
@@ -232,234 +277,90 @@ class MoveGroupPythonIntefaceTutorial(object):
     # Publish
     display_trajectory_publisher.publish(display_trajectory);
 
-    ## END_SUB_TUTORIAL
+    ## END
 
   def execute_plan(self, plan):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
     group = self.group
 
-    ## BEGIN_SUB_TUTORIAL execute_plan
-    ##
     ## Executing a Plan
     ## ^^^^^^^^^^^^^^^^
     ## Use execute if you would like the robot to follow
     ## the plan that has already been computed:
 
     group.execute(plan, wait=True)
-   
+    
     ## **Note:** The robot's current joint state must be within some tolerance of the
     ## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
-    ## END_SUB_TUTORIAL
-
-  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
- 
-    box_name = self.box_name
-    scene = self.scene
-
-    ## BEGIN_SUB_wait_for_scene_update
-    ##
-    ## Ensuring Collision Updates Are Receieved
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## If the Python node dies before publishing a collision object update message, the message
-    ## could get lost and the box will not appear. To ensure that the updates are
-    ## made, we wait until we see the changes reflected in the
-    ## ``get_known_object_names()`` and ``get_known_object_names()`` lists.
-    ## For the purpose of this tutorial, we call this function after adding,
-    ## removing, attaching or detaching an object in the planning scene. We then wait
-    ## until the updates have been made or ``timeout`` seconds have passed
-    start = rospy.get_time()
-    seconds = rospy.get_time()
-    while (seconds - start < timeout) and not rospy.is_shutdown():
-      # Test if the box is in attached objects
-      attached_objects = scene.get_attached_objects([box_name])
-      is_attached = len(attached_objects.keys()) > 0
-
-      # Test if the box is in the scene.
-      # Note that attaching the box will remove it from known_objects
-      is_known = box_name in scene.get_known_object_names()
-      # Test if we are in the expected state
-      if (box_is_attached == is_attached) and (box_is_known == is_known):
-        return True
-
-      # Sleep so that we give other threads time on the processor
-      rospy.sleep(0.1)
-      seconds = rospy.get_time()
-
-    # If we exited the while loop without returning then we timed out
-    return False
-    ## END_SUB_TUTORIAL
-
-  def add_box(self, timeout=4):
-    robot_name = self.robot_name
-    box_name = self.box_name
-    scene = self.scene
-
-    ## BEGIN_SUB_TUTORIAL add_box
-    ##
-    ## Adding Objects to the Planning Scene
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## First, we will create a box in the planning scene at the location of the left finger:
-    box_pose = geometry_msgs.msg.PoseStamped()
-    box_pose.header.frame_id = robot_name + "/ee_gripper_link"
-    box_pose.pose.orientation.w = 1.0
-    box_name = "box"
-    self.scene.add_box(box_name, box_pose, size=(0.025, 0.025, 0.05))
-
-    ## END_SUB_TUTORIAL
-    # Copy local variables back to class variables. In practice, you should use the class
-    # variables directly unless you have a good reason not to.
-    self.box_name=box_name
-    return self.wait_for_state_update(box_is_known=True, timeout=timeout)
-
-
-  def attach_box(self, timeout=4):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    box_name = self.box_name
-    robot = self.robot
-    scene = self.scene
-    eef_link = self.eef_link
-    group_names = self.group_names
-
-    ## BEGIN_SUB_TUTORIAL attach_object
-    ##
-    ## Attaching Objects to the Robot
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## Next, we will attach the box to the Arm's wrist. Manipulating objects requires the
-    ## robot be able to touch them without the planning scene reporting the contact as a
-    ## collision. By adding link names to the ``touch_links`` array, we are telling the
-    ## planning scene to ignore collisions between those links and the box. For the Interbotix
-    ## robot, we set ``grasping_group = 'interbotix_gripper'``. If you are using a different robot,
-    ## you should change this value to the name of your end effector group name.
-    grasping_group = 'interbotix_gripper'
-    touch_links = robot.get_link_names(group=grasping_group)
-    scene.attach_box(eef_link, box_name, touch_links=touch_links)
-    ## END_SUB_TUTORIAL
-
-    # We wait for the planning scene to update.
-    return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
-
-  def detach_box(self, timeout=4):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    box_name = self.box_name
-    scene = self.scene
-    eef_link = self.eef_link
-
-    ## BEGIN_SUB_TUTORIAL detach_object
-    ##
-    ## Detaching Objects from the Robot
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## We can also detach and remove the object from the planning scene:
-    scene.remove_attached_object(eef_link, name=box_name)
-    ## END_SUB_TUTORIAL
-
-    # We wait for the planning scene to update.
-    return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
-
-  def remove_box(self, timeout=4):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    box_name = self.box_name
-    scene = self.scene
-
-    ## BEGIN_SUB_TUTORIAL remove_object
-    ##
-    ## Removing Objects from the Planning Scene
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## We can remove the box from the world.
-    scene.remove_world_object(box_name)
-
-    ## **Note:** The object must be detached before we can remove it from the world
-    ## END_SUB_TUTORIAL
-
-    # We wait for the planning scene to update.
-    return self.wait_for_state_update(box_is_attached=False, box_is_known=False, timeout=timeout)
-
-def scoop(tutorial, j0, j1, j2, j3, j4):
-	#Save j3 and j4 init value
-	initJ3 = j3
-	initJ4 = j4
-
-	#Make j4 pi/2 to twist scoop vertically
-	j4 = -pi/2
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-
-	#Make j3 more negative to move down into bucket
-	j3 = j3 - pi/4
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-
-	#Make j4 init value to do scoop motion and grab object
-	j4 = initJ4
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-
-	#Make j3 init j3 to move up from bucket to where it originally was
-	j3 = initJ3
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-
-	#Move arm to safety position--------------------------------------
-    	j0=-pi/2
-      	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-
-	#Drop object down
-	#j3 = j3 - pi/2
-	#tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-	j4=-pi
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-	j4=initJ4
-	tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
-	return
+    ## END
 
 def main():
   try:
-    print "============ Press `Enter` to begin the tutorial by setting up the moveit_commander (press ctrl-d to exit) ..."
+    print "============ Press `Enter` to begin Water Rescue (press ctrl-d to exit) ..."
     raw_input()
-    tutorial = MoveGroupPythonIntefaceTutorial()
+    Movement = MoveGroupPythonInteface_RoboticArm_Rescue()
 
-    print "============ Initial state"
+    
+    print "============ Press `Enter` to execute a movement using a joint state goal ..."
     j0 = 0
-    j1 = -0.09
-    j2 = 1.3
-    j3 = -1.17
+    j1 = -pi/6
+    j2 = pi/6
+    j3 = -pi/3 
     j4 = 0
-    tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
+    
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
 
-    #plan cartesian path with x and y
-    dx = 7
-    dy = -7
-    dz = 6
-    cartesian_plan, fraction = tutorial.plan_cartesian_path(dx,dy,dz)
-    tutorial.display_trajectory(cartesian_plan)
+    #raw_input()
+    #Movement.go_to_pose_goal()
+
+    print "============ Press `Enter` to plan and display a Cartesian path ..."
+    raw_input()
+    # Input coordinates from Opencv
+    dx = float(input("Enter coordinate x : "))
+    dy = float(input("Enter coordinate y: ")) 
+    dz = 4
+    
+    cartesian_plan, fraction = Movement.plan_cartesian_path((-1)*dx,dy,dz)
+
+    print "============ Press `Enter` to display a saved trajectory (this will replay the Cartesian path)  ..."
+    #raw_input()
+    Movement.display_trajectory(cartesian_plan)
 
     print "============ Cartesian Path to object ..."
-    raw_input()
-    tutorial.execute_plan(cartesian_plan)
+    # Cartesian path to object 
+    Movement.execute_plan(cartesian_plan) 
 
-    #Run the scooping function
+     #Run the scooping function
     print "============ Press 'Enter' to scoop the object"
+    
+    # Scooping right or left based on the y coordinate of the object
     raw_input()
-    group = tutorial.group
-    joint_goal = group.get_current_joint_values()
-    j0 = joint_goal[0]
-    j1 = joint_goal[1]
-    j2 = joint_goal[2]
-    j3 = joint_goal[3]
-    j4 = joint_goal[4]
-    scoop(tutorial, j0, j1, j2, j3, j4)
+    if dy<0:
+       group = Movement.group
+       joint_goal = group.get_current_joint_values()
+       j0 = joint_goal[0]
+       j1 = joint_goal[1]
+       j2 = joint_goal[2]
+       j3 = joint_goal[3]
+       j4 = joint_goal[4]
+       Movement.scoop_right( j0, j1, j2, j3, j4)
+    else:
+       group = Movement.group
+       joint_goal = group.get_current_joint_values()
+       j0 = joint_goal[0]
+       j1 = joint_goal[1]
+       j2 = joint_goal[2]
+       j3 = joint_goal[3]
+       j4 = joint_goal[4]
+       Movement.scoop_left( j0, j1, j2, j3, j4)
 
-    #return to initial state
-    print "============ Initial state"
+    # go back to initial position
     j0 = 0
-    j1 = -0.09
-    j2 = 1.3
-    j3 = -1.17
+    j1 = -pi/6
+    j2 = pi/6
+    j3 = -pi/3 
     j4 = 0
-    tutorial.go_to_joint_state(j0, j1, j2, j3, j4)
+    
+    Movement.go_to_joint_state(j0, j1, j2, j3, j4)
 
     print "============ Trajectory complete!"
   except rospy.ROSInterruptException:
