f177c8fc1717d3f988e14ab5f92a2819df69055b
isaimat <32847184+isaimat@users.noreply.github.com>
2021-03-14 18:02:22 -0700

    Final Update for CVdistance

--- track_blob.py
+++ track_blob.py
@@ -12,7 +12,7 @@ import numpy as np
 
 rospy.init_node("track_blob")
 
-cap=cv2.VideoCapture(1)
+cap=cv2.VideoCapture(0)
 
 
 
@@ -24,8 +24,6 @@ target_pose=Pose() # declaring a message variable of type Int32
 
 x_d=0.0
 y_d=0.0
-x_d_p=0.0
-y_d_p=0.0
 
 while(1):
 	_, img = cap.read()
@@ -33,23 +31,22 @@ while(1):
 	#converting frame(img i.e BGR) to HSV (hue-saturation-value)
 
 	hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-	blue_lower=np.array([94,123,46],np.uint8)
-	blue_upper=np.array([125,255,255],np.uint8)
+	Purple_lower=np.array([120,105,80],np.uint8)# Purple Saturation values
+	Purple_upper=np.array([155,255,255],np.uint8)# Purple Saturation values
 
-
-	blue=cv2.inRange(hsv,blue_lower,blue_upper)
+	Purple=cv2.inRange(hsv,Purple_lower,Purple_upper)
 	
 	#Morphological transformation, Dilation  	
 	kernal = np.ones((5 ,5), "uint8")
 
 
-	blue=cv2.dilate(blue,kernal)
+	Purple=cv2.dilate(Purple,kernal)
 
-	img=cv2.circle(img,(260,68),5,(255,0,0),-1)
+	img=cv2.circle(img,(445,227),5,(0,255,0),-1)# cv2.circle(img,(260,68),5,(255,0,0),-1) 474,245
 
 			
-	#Tracking the Blue Color
-	(_,contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	#Tracking the Purple Color
+	(_,contours,hierarchy)=cv2.findContours(Purple,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 	if len(contours)>0:
 		contour= max(contours,key=cv2.contourArea)
@@ -57,33 +54,23 @@ while(1):
 		if area>800: 
 			x,y,w,h = cv2.boundingRect(contour)	
 			img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-			img=cv2.circle(img,((2*x+w)/2,(2*y+h)/2),5,(255,0,0),-1)
-			img=cv2.line(img,(260,68),((2*x+w)/2,(2*y+h)/2),(0,255,0),2)
+			img=cv2.circle(img,((2*x+w)/2,(2*y+h)/2),5,(0,255,0),-1)
+			img=cv2.line(img,(445,227),((2*x+w)/2,(2*y+h)/2),(0,0,255),2)
 		
-			x_d= (((2*y+h)/2)-68) * 0.06
-			y_d= (((2*x+w)/2)-260) * 0.075
 			
-			s= 'x_d:'+ str(x_d)+ 'y_d:'+str(y_d)
+			y_d= (((2*y+h)/2)-227) * 0.06 
+                        x_d= (((2*x+w)/2)-445) * 0.075 
 			
-			cv2.putText(img,s,(x-20,y-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1,cv2.LINE_AA)
-		
-
-		
-			if (abs(x_d-x_d_p)> 1 or abs(y_d-y_d_p)>1):
-				target_pose.position.x=x_d*0.01
-				target_pose.position.y=y_d*0.01
-				target_pose.position.z=0.0
-				pub.publish(target_pose)
+			s= 'x='+ str(x_d)+'  '+ 'y='+str(y_d)
 			
-				x_d_p=x_d
-				y_d_p=y_d
+			cv2.putText(img,s,(x-20,y-5),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2,cv2.LINE_AA)
 			
 		
-	
-	cv2.imshow("Mask",blue)
+	cv2.imshow("Mask",Purple)
 	cv2.imshow("Color Tracking",img)
 	if cv2.waitKey(1)== ord('q'):
 		break
 
 cap.release()
 cv2.destroyAllWindows()
+
